b10a8621e1a25063accb8f4eb9c8dd66d1c02048
Universe <whdals7979@gmail.com>
2022-08-28 16:09:57 +0900

    POST 요청 및 응답 받기

--- src/main/java/com/example/springbootproject/controller/LoginController.java
+++ src/main/java/com/example/springbootproject/controller/LoginController.java
@@ -1,17 +1,31 @@
 package com.example.springbootproject.controller;
 
+import com.example.springbootproject.domain.MemberForm;
+import com.example.springbootproject.service.MemberService;
+import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/auth")
 public class LoginController{
+    @Autowired
+    MemberService memberService;
 
-    @GetMapping(value="/login")
+    @GetMapping("/auth/login")
     public String showLoginPage(){
         return "login";
+    }
+
+    @PostMapping("/auth/login")
+    @ResponseBody
+    public String processLogin(@RequestBody MemberForm memberForm ){
+        JsonObject json = new JsonObject();
+
+        json.addProperty("userid", memberForm.getUserid());
+        json.addProperty("userpw", memberForm.getUserpw());
+
+        return json.toString();
     }
 }
